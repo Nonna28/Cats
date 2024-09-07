50aa4d1fd2cd359e4d974c7192f86fd5e8831eb3
nonna <nonna28@mail.ru>
2024-09-08 02:25:48 +0400

    Добавили кнопку для загрузки нового изображения

--- Cataas.py
+++ Cataas.py
@@ -3,21 +3,25 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+from pygame.display import update
 
-def loade_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         image_date = BytesIO(response.content)
-        img = Image.open(image_data)
+        img = Image.open( image_date)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
 
-
-
+    if img:
+        label.config(image=img)
+        label.image = img
 
 window = Tk()
 window.title("Cats!")
@@ -26,13 +30,14 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
+
 url = "https://cataas.com/cat"
-img = load_image(url)
 
-if img:
-    label.config(image=img)
-    label.image = img
+set_image()
 
-    window.mainloop()
+
+ window.mainloop()
 
 
